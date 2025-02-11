4a40c13d22bda0ae7e42a49b48598ecb3c187115
acol248 <alex.collyer@acol.dev>
2025-02-11 23:05:36 +0000

    🚧 Add basic authentication

--- server.py
+++ server.py
@@ -1,24 +1,79 @@
 import os
 import re
-from flask import Flask, send_from_directory, request, Response, abort, jsonify
+import jwt
+from flask import Flask, send_from_directory, request, Response, abort, jsonify, make_response
+from werkzeug.security import generate_password_hash, check_password_hash
 from dotenv import load_dotenv
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
+from functools import wraps
 
 load_dotenv(dotenv_path='.env.local')
 
 OUTPUT_DIRECTORY = os.getenv('OUTPUT_DIR', './media')
 STATIC = os.getenv('STATIC', 'client/dist')
 
+AUTH_SECRET = os.getenv('AUTH_SECRET', False)
+ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD', 'SomeSecurePassword')
 
+users = {
+    'admin': generate_password_hash(ADMIN_PASSWORD, method='sha256')
+}
+
+def auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.cookies.get('token')
+        
+        if not token:
+            return jsonify({'data': None, 'error': 'Unauthorised.'}), 401
+        try:            
+            data = jwt.decode(token, AUTH_SECRET, algorithms=['HS256'])
+            user = data['user']
+        except:
+            return jsonify({'data': None, 'error': 'Unauthorised.'}), 401
+        return f(user, *args, **kwargs)
+    
+    return decorated
+
+# Main server function
 def create_server():
-    app = Flask(__name__, static_folder=STATIC, static_url_path='')
+    if (not AUTH_SECRET):
+        raise Exception('AUTH_SECRET is required')
+    
+    app = Flask(__name__, static_folder=STATIC, static_url_path='/')
 
-    @app.route('/')
-    def serve():
-        return send_from_directory(app.static_folder, 'index.html')
+    @app.route('/api/login', methods=['POST'])
+    def login():
+        auth = request.json
+        if not auth or 'username' not in auth or 'password' not in auth:
+            return jsonify({'data': None, 'error': 'Bad request.'}), 400
+
+        username = auth['username']
+        password = auth['password']
+        
+        if username not in users or not check_password_hash(users[username], password):
+            return jsonify({'error': 'Invalid credentials'}), 401
+
+        token = jwt.encode({'user': username, 'exp': datetime.utcnow() + timedelta(hours=1)}, AUTH_SECRET, algorithm='HS256')
+        
+        resp = make_response(jsonify({'data': True, 'error': None}))
+        resp.set_cookie('token', token, httponly=True)
+        return resp
+
+    @app.route('/api/logout', methods=['POST'])
+    def logout():
+        resp = make_response(jsonify({'data': True, 'error': None}))
+        resp.set_cookie('token', '', expires=0)
+        return resp
+
+    @app.route('/api/auth', methods=['GET'])
+    @auth
+    def authCheck(user):
+        return jsonify({'data': user, 'error': None})
 
     @app.route('/api/media', methods=['GET'])
-    def media():
+    @auth
+    def media(user):
         files = os.listdir(OUTPUT_DIRECTORY)
         file_with_stats = []
 
@@ -36,7 +91,8 @@ def create_server():
         return jsonify({"data": file_with_stats, "error": None})
 
     @app.route('/api/video/<filename>')
-    def stream_video(filename):
+    @auth
+    def stream_video(user, filename):
         if not re.match(r'^[\w\-. ]+$', filename):
             abort(400, 'Invalid filename')
 
@@ -90,5 +146,15 @@ def create_server():
         rv.headers.add('Accept-Ranges', 'bytes')
         rv.headers.add('Content-Length', str(length))
         return rv
+
+    @app.route('/')
+    def serve():
+        return send_from_directory(app.static_folder, 'index.html')
+    
+    @app.errorhandler(404)
+    def not_found(e):
+        return app.send_static_file('index.html')
+            
+        
 
     return app
