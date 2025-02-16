093a88877bc15e5a86c28b76b68ac787ed36443a
acol248 <alex.collyer@acol.dev>
2025-02-16 01:00:15 +0000

    ✨ Added Profile page

---
+++ client/src/pages/Profile/Profile.tsx
@@ -0,0 +1,21 @@
+import MainLayout from "../../Layouts/MainLayout";
+
+// styles
+import useClassList, { mapClassesCurried } from "@blocdigital/useclasslist";
+import maps from "./Profile.module.scss";
+
+const mc = mapClassesCurried(maps, true);
+
+export default function Profile() {
+  const classList = useClassList({
+    defaultClass: "profile",
+    maps,
+    string: true,
+  });
+
+  return (
+    <MainLayout className={classList}>
+      <a className={mc('profile__link')} href="/logout">Logout</a>
+    </MainLayout>
+  );
+}
